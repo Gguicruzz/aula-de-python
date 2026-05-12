48bb036a7e26355ba1cdd141f552b9e4d1a20858
Guilherme cruz alves <nestunit980@gmail.comm>
2026-05-11 22:02:08 -0300

    troca de código

--- arq03.py
+++ arq03.py
@@ -10,3 +10,23 @@ with open("arq02.txt","w+", encoding="utf-8") as arq:
     print(0)
     linhas_arq = arq.readlines()
     print(linhas_arq[1])
+
+# Exercio, grave ao menos 4 linhas em um arquivo 
+# peça para o usuario escolher uma linha 
+# - exiba na tela a linha 
+# - conte quantas palavras há na linha 
+# - conte quantos caracteres há na linha      
+
+with open("arq03.txt","w+", encoding="uft-8") as arq: 
+    
+    arq.write("Nova linha")
+    arq.write("Nova linha")
+    arq.write("Nova linha")
+    arq.write("Nova linha")
+    arq.seek(0)
+    escolha = input("escolha uma linha: ")
+    linha = arq.readline(escolha)
+    print(arq.readline())
+    len(linha)
+    linha.count()
+
---
+++ exercio atividade 1 cp6.py
@@ -0,0 +1,100 @@
+#nome: Guilherme cruz alves 
+#rm: 566861
+
+funcionarios = []
+
+while True:
+    print("""
+========================
+        MENU
+========================
+
+0 - SAIR
+1 - CADASTRAR FUNCIONÁRIO
+2 - CONSULTAR FUNCIONÁRIO
+5 - LISTAR FUNCIONÁRIOS
+
+ESCOLHA:
+""", end=" ")
+
+    opcao = input()
+
+    match opcao:
+
+        # SAIR
+        case "0":
+            print("\nEncerrando sistema...")
+            break
+
+        # CADASTRAR
+        case "1":
+            print("\n========================")
+            print(" CADASTRANDO FUNCIONÁRIO")
+            print("========================")
+
+            nome = input("\nNome: ")
+            cpf = input("CPF: ")
+            salario = float(input("Salário: "))
+
+            funcionario = {
+                "nome": nome,
+                "cpf": cpf,
+                "salario": salario
+            }
+
+            funcionarios.append(funcionario)
+
+            print("\nFuncionário cadastrado com sucesso!")
+            input("\nPressione ENTER para continuar...")
+
+        # CONSULTAR
+        case "2":
+            print("\n========================")
+            print(" CONSULTANDO FUNCIONÁRIO")
+            print("========================")
+
+            cpf_busca = input("\nCPF......: ")
+
+            encontrado = False
+
+            for f in funcionarios:
+
+                if f["cpf"] == cpf_busca:
+                    print("\nCPF......:", f["cpf"])
+                    print("Nome.....:", f["nome"])
+                    print("Salário..:", f["salario"])
+
+                    encontrado = True
+                    break
+
+            if encontrado == False:
+                print("\nFuncionário inexistente!")
+
+            input("\nPressione ENTER para continuar...")
+
+        # LISTAR
+        case "5":
+            print("\n========================")
+            print(" LISTA DE FUNCIONÁRIOS")
+            print("========================")
+
+            if len(funcionarios) == 0:
+                print("\nNenhum funcionário cadastrado!")
+
+            else:
+                for i, f in enumerate(funcionarios, 1):
+                    print(f"""
+Funcionário {i}
+
+CPF......: {f['cpf']}
+Nome.....: {f['nome']}
+Salário..: {f['salario']}
+""")
+
+            input("Pressione ENTER para continuar...")
+
+        # OPÇÃO INVÁLIDA
+        case _:
+            print("\nOpção inválida!")
+            input("Pressione ENTER para continuar...")
+            
